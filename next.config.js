363e3cc719152ec0026b979f02380fcd42b44973
insik-han <insikHan@itez.io>
2021-07-14 10:04:19 +0900

    커스텀테마 추가

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-module.exports = {
+const withLess = require("next-with-less");
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    /* ... */
+    lessOptions: {
+      /* ... */
+      // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+      modifyVars: {
+        "primary-color": "hotpink",
+        "layout-header-background": "#ffffff",
+        "layout-trigger-background": "#ffffff",
+        /* ... */
+      },
+    },
+  },
   images: {
     domains: ["images.unsplash.com", "localhost"],
   },
-};
+});
