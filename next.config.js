64e277776b929fdfb1fcbfaccd3a6271f3fdf82b
insik-han <insikHan@itez.io>
2021-07-15 21:28:24 +0900

    번들 아날라이저 추가

--- next.config.js
+++ next.config.js
@@ -1,20 +1,25 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const withLess = require("next-with-less");
 
-module.exports = withLess({
-  lessLoaderOptions: {
-    /* ... */
-    lessOptions: {
+module.exports = withBundleAnalyzer(
+  withLess({
+    lessLoaderOptions: {
       /* ... */
-      // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
-      modifyVars: {
-        "primary-color": "hotpink",
-        "layout-header-background": "#ffffff",
-        "layout-trigger-background": "#ffffff",
+      lessOptions: {
         /* ... */
+        // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+        modifyVars: {
+          "primary-color": "hotpink",
+          "layout-header-background": "#ffffff",
+          "layout-trigger-background": "#ffffff",
+          /* ... */
+        },
       },
     },
-  },
-  images: {
-    domains: ["images.unsplash.com", "localhost"],
-  },
-});
+    images: {
+      domains: ["images.unsplash.com", "localhost"],
+    },
+  })
+);
